f23e4eb00f85629acdae322dc589904922260db7
imrulh <imrulhasan@seekasia.com>
2017-12-13 15:31:34 +0800

    small changes

---
+++ src/components/chat/chat.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import {Launcher} from '../../src'
+
+export default class Chat extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      messageList: []
+    };
+  }
+
+  _onMessageWasSent(message) {
+    this.setState({
+      messageList: [...this.state.messageList, message]
+    })
+  }
+
+  _sendMessage(text) {
+    if (text.length > 0) {
+      this.setState({
+        messageList: [...this.state.messageList, {
+          author: 'them',
+          type: 'text',
+          data: { text }
+        }]
+      })
+    }
+  }
+
+  render() {
+    return (<div>
+      <Launcher
+        agentProfile={{
+          teamName: 'react-live-chat',
+          imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+        }}
+        onMessageWasSent={this._onMessageWasSent.bind(this)}
+        messageList={this.state.messageList}
+      />
+    </div>)
+  }
+}
--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,10 +1,11 @@
 
 import React, { Component } from 'react';
-
+import Chat from './chat';
 class ChatContainer extends Component {
     render() {
         return (
             <div className="ImageBackground" style={{ 'height': window.innerHeight }}>
+                <Chat />
             </div>
         );
     }
